4ce8f616c30c682a4bf8b6b703b6171edd6683a3
KT <blushes-copiers.03@icloud.com>
2025-04-06 22:49:57 +0200

    feat: Add FastApi app and /waves endpoint

--- ReadMe.md
+++ ReadMe.md
@@ -16,3 +16,13 @@ python3 -m pip install -r requirements.txt
   * `processing.py` takes the original dataset and extracts just the largest wave for that day, discarding the 23 hours worth of measurements that were less than or equal to the max.
   * This slims the dataset down from 45 to 2.3mb and makes lookups faster.
   * The results have been saved into `waves_2019-01-01-hmax-max.nc`
+
+## Run the webapp
+
+You can run the app locally like so: 
+
+```sh
+fastapi dev main.py
+```
+
+Then you should be able to view the OpenApi docs for the api at: http://localhost:8000/docs
---
+++ main.py
@@ -0,0 +1,68 @@
+from fastapi import FastAPI
+import xarray as xr
+import pandas as pd
+from pydantic import BaseModel, Field
+from fastapi.responses import HTMLResponse
+from fastapi.exceptions import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "").split(",")
+allowed_origins = [origin.strip() for origin in allowed_origins if origin.strip()]
+
+app = FastAPI()
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+ds = xr.open_dataset("waves_2019-01-01-hmax-max.nc")
+
+class WaveHeight(BaseModel):
+    hmax: float = Field(
+        default=...,
+        description="The maximum wave height for the location",
+        example=3.1415,
+    )
+    time: str = Field(
+        default=...,
+        description="The date and time of the maximum wave height as an ISO formatted string",
+        example="2019-01-01T23:00:00",
+    )
+    latitude: float = Field(
+        default=...,
+        description="The latitude of the data point",
+        example=37.24804,
+    )
+    longitude: float = Field(
+        default=...,
+        description="The longitude of the data point",
+        example=-115.800155,
+    )
+
+
+@app.get("/waves", response_model=WaveHeight, tags=["api"], responses={404: {"description": "No data available for the given location"}})
+def max_hwave(latitude: float, longitude: float):
+    """
+    Gets the height of the highest wave and the time of day it occurred for the location nearest to the given coordinates
+    """
+    v = ds.sel(latitude=latitude, longitude=longitude, method="nearest")
+    hmax = v["hmax"].item()
+    t = pd.Timestamp(v["time"].item()).isoformat()
+    # Get the coordinates of the nearest data point
+    nearest_lat = float(v.latitude.item())
+    nearest_lon = float(v.longitude.item())
+    if pd.isna(hmax):
+        raise HTTPException(
+            status_code=404, detail="No data available for the given location"
+        )
+    return WaveHeight(
+        hmax=hmax,
+        time=t,
+        latitude=nearest_lat,
+        longitude=nearest_lon,
+    )
